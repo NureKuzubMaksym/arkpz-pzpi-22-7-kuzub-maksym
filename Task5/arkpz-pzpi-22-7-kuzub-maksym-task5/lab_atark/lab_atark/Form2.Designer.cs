33544ad41c67c63a49638ac1196605771d8d7837
NureKuzubMaksym <maksym.kuzub1@nure.ua>
2024-12-31 16:55:44 +0200

    Add files via upload

---
+++ Task5/arkpz-pzpi-22-7-kuzub-maksym-task5/lab_atark/lab_atark/Form2.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace lab_atark
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.atarkDataSet = new lab_atark.atarkDataSet();
+            this.tripsBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.tripsTableAdapter = new lab_atark.atarkDataSetTableAdapters.TripsTableAdapter();
+            this.idDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.employeeIdDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.cargoDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.fromAddressDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.toAddressDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.distanceDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.fuelUsedDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.atarkDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tripsBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.idDataGridViewTextBoxColumn,
+            this.employeeIdDataGridViewTextBoxColumn,
+            this.cargoDataGridViewTextBoxColumn,
+            this.fromAddressDataGridViewTextBoxColumn,
+            this.toAddressDataGridViewTextBoxColumn,
+            this.distanceDataGridViewTextBoxColumn,
+            this.fuelUsedDataGridViewTextBoxColumn});
+            this.dataGridView1.DataSource = this.tripsBindingSource;
+            this.dataGridView1.Location = new System.Drawing.Point(1, 1);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(1234, 449);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // atarkDataSet
+            // 
+            this.atarkDataSet.DataSetName = "atarkDataSet";
+            this.atarkDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // tripsBindingSource
+            // 
+            this.tripsBindingSource.DataMember = "Trips";
+            this.tripsBindingSource.DataSource = this.atarkDataSet;
+            // 
+            // tripsTableAdapter
+            // 
+            this.tripsTableAdapter.ClearBeforeFill = true;
+            // 
+            // idDataGridViewTextBoxColumn
+            // 
+            this.idDataGridViewTextBoxColumn.DataPropertyName = "Id";
+            this.idDataGridViewTextBoxColumn.HeaderText = "Id";
+            this.idDataGridViewTextBoxColumn.Name = "idDataGridViewTextBoxColumn";
+            this.idDataGridViewTextBoxColumn.ReadOnly = true;
+            this.idDataGridViewTextBoxColumn.Width = 41;
+            // 
+            // employeeIdDataGridViewTextBoxColumn
+            // 
+            this.employeeIdDataGridViewTextBoxColumn.DataPropertyName = "EmployeeId";
+            this.employeeIdDataGridViewTextBoxColumn.HeaderText = "EmployeeId";
+            this.employeeIdDataGridViewTextBoxColumn.Name = "employeeIdDataGridViewTextBoxColumn";
+            this.employeeIdDataGridViewTextBoxColumn.Width = 87;
+            // 
+            // cargoDataGridViewTextBoxColumn
+            // 
+            this.cargoDataGridViewTextBoxColumn.DataPropertyName = "Cargo";
+            this.cargoDataGridViewTextBoxColumn.HeaderText = "Cargo";
+            this.cargoDataGridViewTextBoxColumn.Name = "cargoDataGridViewTextBoxColumn";
+            this.cargoDataGridViewTextBoxColumn.Width = 60;
+            // 
+            // fromAddressDataGridViewTextBoxColumn
+            // 
+            this.fromAddressDataGridViewTextBoxColumn.DataPropertyName = "FromAddress";
+            this.fromAddressDataGridViewTextBoxColumn.HeaderText = "FromAddress";
+            this.fromAddressDataGridViewTextBoxColumn.Name = "fromAddressDataGridViewTextBoxColumn";
+            this.fromAddressDataGridViewTextBoxColumn.Width = 93;
+            // 
+            // toAddressDataGridViewTextBoxColumn
+            // 
+            this.toAddressDataGridViewTextBoxColumn.DataPropertyName = "ToAddress";
+            this.toAddressDataGridViewTextBoxColumn.HeaderText = "ToAddress";
+            this.toAddressDataGridViewTextBoxColumn.Name = "toAddressDataGridViewTextBoxColumn";
+            this.toAddressDataGridViewTextBoxColumn.Width = 83;
+            // 
+            // distanceDataGridViewTextBoxColumn
+            // 
+            this.distanceDataGridViewTextBoxColumn.DataPropertyName = "Distance";
+            this.distanceDataGridViewTextBoxColumn.HeaderText = "Distance";
+            this.distanceDataGridViewTextBoxColumn.Name = "distanceDataGridViewTextBoxColumn";
+            this.distanceDataGridViewTextBoxColumn.Width = 74;
+            // 
+            // fuelUsedDataGridViewTextBoxColumn
+            // 
+            this.fuelUsedDataGridViewTextBoxColumn.DataPropertyName = "FuelUsed";
+            this.fuelUsedDataGridViewTextBoxColumn.HeaderText = "FuelUsed";
+            this.fuelUsedDataGridViewTextBoxColumn.Name = "fuelUsedDataGridViewTextBoxColumn";
+            this.fuelUsedDataGridViewTextBoxColumn.Width = 77;
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1237, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form2_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.atarkDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tripsBindingSource)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private atarkDataSet atarkDataSet;
+        private System.Windows.Forms.BindingSource tripsBindingSource;
+        private atarkDataSetTableAdapters.TripsTableAdapter tripsTableAdapter;
+        private System.Windows.Forms.DataGridViewTextBoxColumn idDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn employeeIdDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn cargoDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn fromAddressDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn toAddressDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn distanceDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn fuelUsedDataGridViewTextBoxColumn;
+    }
+}
